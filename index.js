65dfd94951639bcba2117d087d2fba4704b120a3
Ankit Lall <alallds70@gmail.com>
2021-06-03 00:17:51 +0530

    cors

--- index.js
+++ index.js
@@ -13,13 +13,18 @@ const AssociatedZipModel = require('./Model/AssociatedZip');
 
 const Calculator = require('./Calculator');
 
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 mongoose.connect(uri,{
     useNewUrlParser: true,
